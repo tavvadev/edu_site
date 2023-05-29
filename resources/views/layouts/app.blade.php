d919567489bb13478bb35fa8bba3f4e3e17935af
shiva129 <shiva.dothi@gmail.com>
2023-05-29 08:53:29 +0530

    UpdatesLayout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,7 @@
                         <li class="nav-item">
                           <a class="nav-link active" href="javascript:void(0)">Home</a>
                         </li>
-                        <li class="nav-item">
+                       <!--  <li class="nav-item">
                           <a class="nav-link" href="javascript:void(0)">MIS Report</a>
                         </li>
                         <li class="nav-item">
@@ -61,22 +61,45 @@
                         </li>
                         <li class="nav-item">
                           <a class="nav-link" href="javascript:void(0)">Documents</a>
-                        </li>
+                        </li> -->
                       </ul>
 
                       <ul class="navbar-nav ">
                       
-                     @if (Route::has('login'))
+                      @guest
+                            @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a role="button " class="btn btn-secondary px-4 fw-normal" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a role="button " class="btn btn-secondary px-4 fw-normal" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
+                        @else
+                            <li><a class="nav-link" href="{{ route('users.index') }}">Manage Users</a></li>
+                            <li><a class="nav-link" href="{{ route('roles.index') }}">Manage Role</a></li>
+                            <li><a class="nav-link" href="{{ route('products.index') }}">Manage Product</a></li>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
                 
                       </ul>
 
