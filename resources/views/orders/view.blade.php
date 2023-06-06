b96e40a1c00e017380fe7c28049c4bb03bb48f77
tavvadev <31776776+tavvadev@users.noreply.github.com>
2023-06-07 01:57:13 +0530

    View order

---
+++ resources/views/orders/view.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.master')
+
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> View order</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                {{ $orderDetails->orderId }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Details:</strong>
+                {{ $orderDetails->school_name }}
+            </div>
+        </div>
+    </div>
+@endsection
+<p class="text-center text-primary"><small>Tutorial by edutechsolutions</small></p>
