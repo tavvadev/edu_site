50ab0c52ddc6ca9287b40f71c9d7e9402c610538
tavvadev <31776776+tavvadev@users.noreply.github.com>
2023-06-07 00:47:39 +0530

    update school form data

---
+++ resources/views/users/updateschool.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Create New User</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('users.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+
+<h1>Update School Information</h1>
+
+@endsection
