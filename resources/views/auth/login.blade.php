f0aabe3a155a03133ba82ffb8b3ad11850fdf57f
chakri5657 <vennapu.chakri@medichire.ai>
2023-06-07 06:43:24 +0530

    loginpage

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,28 +1,40 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<div class="container-fluid login-pg">
 
-                <div class="card-body">
+        <div class="container pt-5 mt-5 pb-5 mb-4">
+
+        <div class="card" >
+
+
+        <div class="row justify-content-center ">
+        <div class="col-md-6">
+            <img src="assets/images/campus.jpg" style="width:100%;min-height:520px;object-fit:cover;" width="400" height="400" class="img-fluid"/>
+        </div>
+        <div class="col-md-6">
+        <div class=" form">
+        <div class="p-4">
+                <h2 class="fs-4 fw-bold title-clr text-center pb-5">{{ __('Login') }}</h2>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Login ID') }}</label>
+                            <div class="form-group">
 
-                            <div class="col-md-6">
+
+
+                            <label for="email" class="fw-bold pb-1">{{ __('Login ID') }}</label>
+
+
                                 <input id="login_id" type="text" class="form-control @error('login_id') is-invalid @enderror" name="login_id" value="{{ old('login_id') }}" required autocomplete="login_id" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                    <p class="text-danger " role="alert">
+                                        <small>{{ $message }}</small>
+</p>
                                 @enderror
-                            </div>
+
+                        </div>
                         </div>
 
                         <div class="row mb-3">
@@ -65,9 +77,12 @@
                             </div>
                         </div>
                     </form>
-                </div>
-            </div>
+
+                    </div>
         </div>
-    </div>
+        </div>
+        </div>
+        </div>
+        </div>
 </div>
 @endsection
