b92e54578f11261b276e0a6cff832f4d0687168d
chakri5657 <vennapu.chakri@medichire.ai>
2023-06-07 08:05:05 +0530

    login page completed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,25 +9,21 @@
 
 
         <div class="row justify-content-center ">
-        <div class="col-md-6">
+        <div class="col-md-6 p-0">
             <img src="assets/images/campus.jpg" style="width:100%;min-height:520px;object-fit:cover;" width="400" height="400" class="img-fluid"/>
         </div>
-        <div class="col-md-6">
-        <div class=" form">
-        <div class="p-4">
-                <h2 class="fs-4 fw-bold title-clr text-center pb-5">{{ __('Login') }}</h2>
+        <div class="col-md-6 p-0">
+
+        <div class="form-container">
+
+        <div class="inner-cont">
+        <h2 class="fs-4 fw-bold title-clr text-center pb-5">{{ __('Login') }}</h2>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-                        <div class="row mb-3">
-                            <div class="form-group">
 
-
-
+                            <div class="form-group mb-4">
                             <label for="email" class="fw-bold pb-1">{{ __('Login ID') }}</label>
-
-
-                                <input id="login_id" type="text" class="form-control @error('login_id') is-invalid @enderror" name="login_id" value="{{ old('login_id') }}" required autocomplete="login_id" autofocus>
-
+                               <input id="login_id" type="text" class="form-control @error('login_id') is-invalid @enderror" name="login_id" value="{{ old('login_id') }}" required autocomplete="login_id" autofocus>
                                 @error('email')
                                     <p class="text-danger " role="alert">
                                         <small>{{ $message }}</small>
@@ -35,37 +31,36 @@
                                 @enderror
 
                         </div>
-                        </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+
+                        <div class="form-group mb-4">
+                            <label    for="email" class="fw-bold pb-1">{{ __('Password') }}</label>
+
+
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                    <p class="invalid-feedback" role="alert">
+                                        <small>{{ $message }}</small>
+</p>
                                 @enderror
-                            </div>
+
                         </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
+                                    <input class="form-check-input" type="checkbox"
+                                    name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                                     <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
                                     </label>
                                 </div>
-                            </div>
-                        </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+
+
+                        <div class="row my-4 pt-4">
+                            <div class="col-md-6 offset-md-3">
+                                <button type="submit" class="btn py-3 w-100 btn-primary">
                                     {{ __('Login') }}
                                 </button>
 
