ca6e25e1c7a0764aea93051852fe6e95a6634005
shiva129 <shiva.dothi@gmail.com>
2023-06-14 20:24:37 +0530

    Profile

--- resources/views/users/schoolprofile.blade.php
+++ resources/views/users/schoolprofile.blade.php
@@ -26,60 +26,82 @@
             <?php $i=0; ?>
             <input type="hidden" name="school_id" value="{{$schoolDetails['id']}}" />
             <div class="col-md-12">
-                <div class="form-group border-bottom pb-3 pt-3 d-flex align-items-center justify-content-between mb-2">
-                    <label class="fw-bold col-md-8">School Name </label>
-                    <div class="col-md-4 d-flex align-items-center">
+            <div class="form-group border-bottom pb-3 pt-3 d-flex align-items-center justify-content-between mb-2">
+                    <label class="fw-bold col-md-6">School ID </label>
+                    <div class="col-md-6 d-flex align-items-center">
                         {{$schoolDetails['school_name']}}
                     </div>
                 </div>
                 <div class="form-group border-bottom pb-3 pt-3 d-flex align-items-center justify-content-between mb-2">
-                    <label class="fw-bold col-md-8">Login ID </label>
-                    <div class="col-md-4 d-flex align-items-center">
+                    <label class="fw-bold col-md-6">School Name </label>
+                    <div class="col-md-6 d-flex align-items-center">
+                        {{$schoolDetails['school_name']}}
+                    </div>
+                </div>
+                <div class="form-group border-bottom pb-3 pt-3 d-flex align-items-center justify-content-between mb-2">
+                    <label class="fw-bold col-md-6">No Of Teachers </label>
+                    <div class="col-md-6 d-flex align-items-center">
+                        {{$schoolDetails['school_name']}}
+                    </div>
+                </div>
+                <div class="form-group border-bottom pb-3 pt-3 d-flex align-items-center justify-content-between mb-2">
+                    <label class="fw-bold col-md-6">No Of Students </label>
+                    <div class="col-md-6 d-flex align-items-center">
+                        {{$schoolDetails['school_name']}}
+                    </div>
+                </div>
+                <div class="form-group border-bottom pb-3 pt-3 d-flex align-items-center justify-content-between mb-2">
+                    <label class="fw-bold col-md-6">No Of  Class rooms </label>
+                    <div class="col-md-6 d-flex align-items-center">
+                        {{$schoolDetails['school_name']}}
+                    </div>
+                </div>
+                <div class="form-group border-bottom pb-3 pt-3 d-flex align-items-center justify-content-between mb-2">
+                    <label class="fw-bold col-md-6">School Address </label>
+                    <div class="col-md-6 d-flex align-items-center">
+                        {{$schoolDetails['school_name']}}
+                    </div>
+                </div>
+                <div class="form-group border-bottom pb-3 pt-3 d-flex align-items-center justify-content-between mb-2">
+                    <label class="fw-bold col-md-6">PIN Code </label>
+                    <div class="col-md-6 d-flex align-items-center">
+                        {{$schoolDetails['school_name']}}
+                    </div>
+                </div>
+                <div class="form-group border-bottom pb-3 pt-3 d-flex align-items-center justify-content-between mb-2">
+                    <label class="fw-bold col-md-6">School Location </label>
+                    <div class="col-md-6 d-flex align-items-center">
+                        {{$schoolDetails['school_name']}}
+                    </div>
+                </div>
+                <div class="form-group border-bottom pb-3 pt-3 d-flex align-items-center justify-content-between mb-2">
+                    <label class="fw-bold col-md-6">Head Master Name </label>
+                    <div class="col-md-6 d-flex align-items-center">
+                        {{$schoolDetails['school_name']}}
+                    </div>
+                </div>
+                <div class="form-group border-bottom pb-3 pt-3 d-flex align-items-center justify-content-between mb-2">
+                    <label class="fw-bold col-md-6">Head Master Contact Number</label>
+                    <div class="col-md-6 d-flex align-items-center">
                         {{$schoolDetails['login_id']}}
                     </div>
                 </div>
                 <div class="form-group border-bottom pb-3 pt-3 d-flex align-items-center justify-content-between mb-2">
-                    <label class="fw-bold col-md-8">School Location </label>
-                    <div class="col-md-4 d-flex align-items-center">
-                        {{$schoolDetails['latitude']}} - {{$schoolDetails['longitude']}}
-                    </div>
-                </div>
-            <div class="col-md-12">
-                <div class="form-group border-bottom pb-3 pt-3 d-flex align-items-center justify-content-between mb-2">
-                    <label class="fw-bold col-md-8">Contact Number </label>
-                    <div class="col-md-4 d-flex align-items-center">
-                        <input type="text" value="{{$schoolDetails['hm_contact_num']}}" id="contact_nuber" name="contact_nuber" class="form-control" placeholder="HM contact">
-                    </div>
-
-                </div>
-            </div>
-            <div class="col-md-12">
-                <div class="form-group border-bottom pb-3 pt-3 d-flex align-items-center justify-content-between mb-2">
-                    <label class="fw-bold col-md-8">Head Master Name </label>
-                    <div class="col-md-4 d-flex align-items-center">
-                        <input type="text"  value="{{$schoolDetails['hm_name']}}" id="contact_nuber" name="contact_nuber" class="form-control" placeholder="HM contact">
-                    </div>
-
-                </div>
-            </div>
-            <div class="col-md-12">
-                <div class="form-group border-bottom pb-3 pt-3 d-flex align-items-center justify-content-between mb-2">
-                    <label class="fw-bold col-md-8">Engineer Name </label>
-                    <div class="col-md-4 d-flex align-items-center">
+                    <label class="fw-bold col-md-6">Alternate Person Name </label>
+                    <div class="col-md-6 d-flex align-items-center">
                         <input type="text" value="{{$schoolDetails['eng_name']}}" id="contact_nuber" name="contact_nuber" class="form-control" placeholder="Eng Name">
                     </div>
-
                 </div>
-            </div>
             <div class="col-md-12">
                 <div class="form-group border-bottom pb-3 pt-3 d-flex align-items-center justify-content-between mb-2">
-                    <label class="fw-bold col-md-8">Engineer Contact No </label>
-                    <div class="col-md-4 d-flex align-items-center">
+                    <label class="fw-bold col-md-6">Alternative Person Contact Number </label>
+                    <div class="col-md-6 d-flex align-items-center">
                         <input type="text" value="{{$schoolDetails['eng_contact']}}" id="contact_nuber" name="contact_nuber" class="form-control" placeholder="Eng contact number">
                     </div>
 
                 </div>
             </div>
+           
 		    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
 		            <button type="submit" class="btn btn-primary mt-3 px-4 py-3">Submit</button>
 		    </div>
