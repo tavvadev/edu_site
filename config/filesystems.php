5554d71e69ec08c10742aaff5fd3faca403e1464
tavvadev <31776776+tavvadev@users.noreply.github.com>
2023-06-18 00:26:30 +0530

    change file system

--- config/filesystems.php
+++ config/filesystems.php
@@ -35,13 +35,11 @@ return [
             'root' => storage_path('app'),
             'throw' => false,
         ],
-
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path(),
+            'url' => env('APP_URL').'/uploads',
             'visibility' => 'public',
-            'throw' => false,
         ],
 
         's3' => [
