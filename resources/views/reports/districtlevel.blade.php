5f888f913aa87dae2c83797229b47c49de883ac5
sivareddymuthuru <sivareddy.muthuru@medichire.ai>
2023-07-02 12:10:35 +0530

    changes in filter

--- resources/views/reports/districtlevel.blade.php
+++ resources/views/reports/districtlevel.blade.php
@@ -24,7 +24,7 @@
   <div class="col-sm-3">
     <div class="input-group">
       <div class="input-group-text">Mandals</div>
-      <select class="form-select" id="mandalOptionsList" name="mandalOptionsList">
+      <select class="form-select" id="mandalOptionsList" name="mandalOptionsList" onchange="villagesList();">
      
     </select>
     </div>
@@ -34,23 +34,20 @@
   <div class="col-sm-3">
     <div class="input-group">
       <div class="input-group-text">Villages</div>
-      <select class="form-select" id="specificSizeSelect">
-      <option selected>Choose...</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-    </select>
+      <select class="form-select" id="village_id" name="village_id" onchange="schoolsList();" >
+      
+      </select>
     </div>
   </div>
 
   <div class="col-sm-3">
     <div class="input-group">
       <div class="input-group-text">School Category</div>
-      <select class="form-select" id="specificSizeSelect">
-      <option selected>Choose...</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
+      <select class="form-select" id="school_category" name="school_category">
+          <option selected>Choose...</option>
+          <option value="PRIMARY">PRIMARY</option>
+          <option value="UPPER PRIMARY">UPPER PRIMARY</option>
+          <option value="SECOUNDARY">SECOUNDARY</option>
     </select>
     </div>
   </div>
@@ -58,12 +55,9 @@
   <div class="col-sm-3">
     <div class="input-group">
       <div class="input-group-text">Schools</div>
-      <select class="form-select" id="specificSizeSelect">
-      <option selected>Choose...</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-    </select>
+      <select class="form-select" id="school_id" name="school_id" >
+      
+      </select>
     </div>
   </div>
 
@@ -106,5 +100,65 @@
             
             
           } 
+
+          function villagesList(){   
+            var villageoptions =""; 
+
+            $.ajax({
+                type: "POST",
+                url: 'http://127.0.0.1:8000/api/villages',
+                contentType: "application/json",
+                dataType: "json",
+                data: JSON.stringify({
+                  district_id: $("#district_id").val(),
+                  mandal_id: $("#mandalOptionsList").val()
+                }),
+                success: function(response) {
+                  villageoptions+='<option selected>Choose...</option>';
+                    var n=0;
+                    $.each(response.villages, function(key, value) {
+                      villageoptions+='<option value="'+response.villages[n].id+'">'+response.villages[n].village_name+'</option>';
+                      n++;
+                    });
+                    $("#village_id").html(villageoptions);
+                },
+                error: function(response) {
+                    console.log(response);
+                }
+            });
+            
+            
+          }
+
+
+          function schoolsList(){   
+            var schoolsoptions =""; 
+
+            $.ajax({
+                type: "POST",
+                url: 'http://127.0.0.1:8000/api/schools',
+                contentType: "application/json",
+                dataType: "json",
+                data: JSON.stringify({
+                  district_id: $("#district_id").val(),
+                  village_id: $("#village_id").val()
+                }),
+                success: function(response) {
+                  schoolsoptions+='<option selected>Choose...</option>';
+                    var p=0;
+                    $.each(response.schools, function(key, value) {
+                      schoolsoptions+='<option value="'+response.schools[p].id+'">'+response.schools[p].school_name+'</option>';
+                      p++;
+                    });
+                    $("#school_id").html(schoolsoptions);
+                },
+                error: function(response) {
+                    console.log(response);
+                }
+            });
+            
+            
+          }
+
 </script>  
 @endsection
