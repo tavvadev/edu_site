f9d8dfd0ecaac7282e4623dea1e73f59fbccdc01
sivareddymuthuru <sivareddy.muthuru@medichire.ai>
2023-07-02 17:19:40 +0530

    district level complete

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -221,8 +221,19 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-<script >
 
+  <?php
+  if(str_contains(url()->current(), '/districtlevelreports')) {
+  ?>
+  <script>
+      $( document ).ready(function() {
+        Reports();
+      });
+  </script>
+  <?php
+  }
+  ?>
+<script >
   $('#owl-carousel').owlCarousel({
     loop:true,
       margin:20,
--- resources/views/reports/districtlevel.blade.php
+++ resources/views/reports/districtlevel.blade.php
@@ -6,12 +6,12 @@
 <?php
         
         ?>
-<form class="row gx-3 gy-2 align-items-center">
+    <link href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" rel="stylesheet"><form class="row gx-3 gy-2 align-items-center">
   <div class="col-sm-3">
   <div class="input-group">
     <div class="input-group-text">Districts</div>
       <select class="form-select" id="district_id" name="district_id" onchange="mandalsList();">
-        <option >Choose...</option>
+        <option value="">Choose...</option>
         <?php
           foreach($districts as $district){
         ?>
@@ -65,100 +65,167 @@
   
   
   <div class="col-auto">
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="button" class="btn btn-primary" onClick="Reports();" >Submit</button>
   </div>
 </form>
 
 <br>
 <br>
+
+<table class="table table-bordered" id="districtreporttable">
+    <tr>
+      <th>S.No</th>
+      <th>District</th>
+      <th>No Of Mandals</th>
+      <th>No Of Villages</th>
+      <th>No Of Schools</th>
+      <th>No Of Teachers</th>
+      <th>No Of Boys</th>
+      <th>No Of Girls</th>
+      <th>Total Students</th>
+      <th>No Of ClassRooms</th>
+    </tr>
+
+</table>
+
+
 <br>
 <script>
-          function mandalsList(){   
-            var mandalsoptions =""; 
+  
+function mandalsList(){   
+  var mandalsoptions =""; 
 
-            $.ajax({
-                type: "POST",
-                url: 'http://127.0.0.1:8000/api/mandals',
-                contentType: "application/json",
-                dataType: "json",
-                data: JSON.stringify({
-                  district: $("#district_id").val()
-                }),
-                success: function(response) {
-                  mandalsoptions+='<option selected>Choose...</option>';
-                    var m=0;
-                    $.each(response.mandals, function(key, value) {
-                      mandalsoptions+='<option value="'+response.mandals[m].id+'">'+response.mandals[m].mandal_name+'</option>';
-                      m++;
-                    });
-                    $("#mandalOptionsList").html(mandalsoptions);
-                },
-                error: function(response) {
-                    console.log(response);
-                }
-            });
-            
-            
-          } 
+  $.ajax({
+      type: "POST",
+      url: 'http://127.0.0.1:8000/api/mandals',
+      contentType: "application/json",
+      dataType: "json",
+      data: JSON.stringify({
+        district: $("#district_id").val()
+      }),
+      success: function(response) {
+        mandalsoptions+='<option value="">Choose...</option>';
+          var m=0;
+          $.each(response.mandals, function(key, value) {
+            mandalsoptions+='<option value="'+response.mandals[m].id+'">'+response.mandals[m].mandal_name+'</option>';
+            m++;
+          });
+          $("#mandalOptionsList").html(mandalsoptions);
+      },
+      error: function(response) {
+          console.log(response);
+      }
+  });
+} 
 
-          function villagesList(){   
-            var villageoptions =""; 
+function villagesList(){   
+  var villageoptions =""; 
 
-            $.ajax({
-                type: "POST",
-                url: 'http://127.0.0.1:8000/api/villages',
-                contentType: "application/json",
-                dataType: "json",
-                data: JSON.stringify({
-                  district_id: $("#district_id").val(),
-                  mandal_id: $("#mandalOptionsList").val()
-                }),
-                success: function(response) {
-                  villageoptions+='<option selected>Choose...</option>';
-                    var n=0;
-                    $.each(response.villages, function(key, value) {
-                      villageoptions+='<option value="'+response.villages[n].id+'">'+response.villages[n].village_name+'</option>';
-                      n++;
-                    });
-                    $("#village_id").html(villageoptions);
-                },
-                error: function(response) {
-                    console.log(response);
-                }
-            });
-            
-            
-          }
+  $.ajax({
+      type: "POST",
+      url: 'http://127.0.0.1:8000/api/villages',
+      contentType: "application/json",
+      dataType: "json",
+      data: JSON.stringify({
+        district_id: $("#district_id").val(),
+        mandal_id: $("#mandalOptionsList").val()
+      }),
+      success: function(response) {
+        villageoptions+='<option value="">Choose...</option>';
+          var n=0;
+          $.each(response.villages, function(key, value) {
+            villageoptions+='<option value="'+response.villages[n].id+'">'+response.villages[n].village_name+'</option>';
+            n++;
+          });
+          $("#village_id").html(villageoptions);
+      },
+      error: function(response) {
+          console.log(response);
+      }
+  });
+  
+  
+}
 
 
-          function schoolsList(){   
-            var schoolsoptions =""; 
+function schoolsList(){   
+  var schoolsoptions =""; 
 
-            $.ajax({
-                type: "POST",
-                url: 'http://127.0.0.1:8000/api/schools',
-                contentType: "application/json",
-                dataType: "json",
-                data: JSON.stringify({
-                  district_id: $("#district_id").val(),
-                  village_id: $("#village_id").val()
-                }),
-                success: function(response) {
-                  schoolsoptions+='<option selected>Choose...</option>';
-                    var p=0;
-                    $.each(response.schools, function(key, value) {
-                      schoolsoptions+='<option value="'+response.schools[p].id+'">'+response.schools[p].school_name+'</option>';
-                      p++;
-                    });
-                    $("#school_id").html(schoolsoptions);
-                },
-                error: function(response) {
-                    console.log(response);
-                }
-            });
-            
-            
-          }
+  $.ajax({
+      type: "POST",
+      url: 'http://127.0.0.1:8000/api/schools',
+      contentType: "application/json",
+      dataType: "json",
+      data: JSON.stringify({
+        district_id: $("#district_id").val(),
+        village_id: $("#village_id").val()
+      }),
+      success: function(response) {
+        schoolsoptions+='<option value="">Choose...</option>';
+          var p=0;
+          $.each(response.schools, function(key, value) {
+            schoolsoptions+='<option value="'+response.schools[p].id+'">'+response.schools[p].school_name+'</option>';
+            p++;
+          });
+          $("#school_id").html(schoolsoptions);
+      },
+      error: function(response) {
+          console.log(response);
+      }
+  });
+  
+  
+}
 
+
+function Reports(){
+  $("#districtreporttable").append('');
+  var reportsHtml =""; 
+  var district_id = $("#district_id").val();
+  if(district_id==""){
+    district_id = "";
+  }
+  var mandal_id = $("#mandalOptionsList").val();
+  if(mandal_id=="" || village_id==null){
+    mandal_id = "";
+  }
+  var village_id = $("#village_id").val();
+  if(village_id=="" || village_id==null){
+    village_id = "";
+  }
+  var school_id = $("#school_id").val();
+  if(school_id=="" || school_id==null){
+    school_id = "";
+  }
+
+  $.ajax({
+      type: "POST",
+      url: 'http://127.0.0.1:8000/api/districtlevelreport',
+      contentType: "application/json",
+      dataType: "json",
+      data: JSON.stringify({
+        district_id: district_id,
+        mandal_id: mandal_id,
+        village_id: village_id,
+        school_id: school_id
+      }),
+      success: function(response) {
+          var p=0;
+          var ml=0;
+          $.each(response.reports, function(key, value) {
+            //console.log("key : "+key+" ; value : "+value);
+
+           reportsHtml+='<tr><td>'+ml+'</td><td>'+response.reports[p].dist_name+'</td><td>'+response.reports[p].mandals_count+'</td><td>'+response.reports[p].villages_count+'</td><td>'+response.reports[p].schools_count+'</td><td>'+response.reports[p].teachers+'</td><td>'+response.reports[p].boys+'</td><td>'+response.reports[p].girls+'</td><td>'+response.reports[p].total_students+'</td><td>'+response.reports[p].total_classrooms+'</td></tr>';
+           p++;
+          });
+          $("#districtreporttable").append(reportsHtml);
+      },
+      error: function(response) {
+          console.log(response);
+      }
+  });
+  
+  
+}
 </script>  
 @endsection
